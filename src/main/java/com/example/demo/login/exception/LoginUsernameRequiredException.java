eb94b1ec4659ee960ccf779ec38edf33d2d95d55
Rohan <rohan.soudey@gmail.com>
2021-08-09 15:29:11 +0530

    added exception conditions

---
+++ src/main/java/com/example/demo/login/exception/LoginUsernameRequiredException.java
@@ -0,0 +1,10 @@
+package com.example.demo.login.exception;
+
+public class LoginUsernameRequiredException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/main/java/com/example/demo/user/constant/Constant.java
+++ src/main/java/com/example/demo/user/constant/Constant.java
@@ -9,4 +9,10 @@ public class Constant {
 	public static final String LOGIN_CREDS_INVALID_EXCEPTION = "Entered username and password does not met.";
 	public static final String LOGIN_USERNAME_ALREADY_EXISTS_EXCEPTION = "Entered username already exists. Please try with different username";
 	public static final String LOGIN_USER_ALREADY_MAPPED_WITH_CREDS_EXCEPTION = "User already mapped with credentials.";
+	public static final String LOGIN_USER_NOT_MAPPED_WITH_CREDS_EXCEPTION = "User field required.";
+	public static final String LOGIN_USER_CANNOT_BE_UPDATED = "User cannot be updated. Please do not pass user object in body.";
+	public static final String LOGIN_ID_DOES_NOT_EXISTS = "Login id does not exists in database.";
+	public static final String LOGIN_ID_REQUIRED = "Login id is required for PUT call";
+	public static final String LOGIN_USERNAME_REQUIRED = "Username required.";
+	public static final String LOGIN_PASSWORD_REQUIRED = "Password required.";
 }
